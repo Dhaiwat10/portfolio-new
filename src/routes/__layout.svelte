680023834573bad419c88a57f71608f7e20720c6
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-09-14 02:37:36 +0530

    Setup tailwind

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
 	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
+
+	const description =
+		'I am Dhaiwat - a 20-year-old software engineer skilled with React, TypeScript, Web3 and more!';
 </script>
+
+<svelte:head>
+	<meta property="og:image" content="/thumb.png" />
+	<meta name="description" content={description} />
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@dhaiwat10" />
+	<meta name="twitter:creator" content="@dhaiwat10" />
+	<meta name="twitter:title" content="Dhaiwat Pandya - Software Engineer" />
+	<meta name="twitter:description" content={description} />
+	<meta name="twitter:image" content='/thumb.png' />
+
+	<title>Dhaiwat Pandya</title>
+	<link rel="icon" href='/favicon.png' />
+</svelte:head>
 
 <Header />
 
@@ -9,11 +28,14 @@
 	<slot />
 </main>
 
-<footer>
+<!-- <footer>
 	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+</footer> -->
 
 <style>
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
 	main {
 		flex: 1;
 		display: flex;
@@ -23,23 +45,5 @@
 		max-width: 1024px;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
 	}
 </style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	mode: 'jit',
+	purge: ['./src/**/*.svelte'],
+	theme: {
+		colors: {
+			black: '#000',
+			white: '#fff',
+			blue: '#1985A1',
+			'accented-black': '#111'
+		}
+	}
+};
